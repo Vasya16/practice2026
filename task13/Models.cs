3c53cdced001b007ccaed8116ac70e6ec19b83bc
Vasya16 <plahinavasilisa@gmail.com>
2026-07-18 05:25:24 +0600

    Реализована JSON-сериализация в classlib с кастомным форматированием, валидацией и тестами

---
+++ task13/Models.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace task13;
+
+public class Subject
+{
+    public string Name { get; set; } = null!;
+    public int Grade { get; set; }
+}
+
+public class Student
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+
+    [JsonConverter(typeof(CustomDateTimeConverter))]
+    public DateTime BirthDate { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public List<Subject>? Grades { get; set; }
+}
+
+public class CustomDateTimeConverter : JsonConverter<DateTime>
+{
+    private const string Format = "yyyy-MM-dd";
+
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateTime.ParseExact(reader.GetString()!, Format, null);
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(Format));
+    }
+}
---
+++ task13tests/SerializationTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+using task13;
+
+namespace task13tests;
+
+public class SerializationTests
+{
+    [Fact]
+    public void Serialize_ShouldIgnoreNullGradesAndFormatDate()
+    {
+        var student = new Student
+        {
+            FirstName = "Иван",
+            LastName = "Иванов",
+            BirthDate = new DateTime(2005, 5, 12),
+            Grades = null
+        };
+
+        string json = StudentSerializer.Serialize(student);
+
+        Assert.Contains("\"BirthDate\": \"2005-05-12\"", json);
+        Assert.DoesNotContain("\"Grades\"", json);
+    }
+
+    [Fact]
+    public void Deserialize_WithInvalidData_ShouldThrowArgumentException()
+    {
+        string invalidJson = "{\"FirstName\":\"\",\"LastName\":\"Петров\",\"BirthDate\":\"2004-01-01\"}";
+        Assert.Throws<ArgumentException>(() => StudentSerializer.DeserializeWithValidation(invalidJson));
+    }
+
+    [Fact]
+    public void SaveAndLoadFromFile_ShouldPreserveData()
+    {
+        string tempFile = Path.GetTempFileName();
+        var student = new Student
+        {
+            FirstName = "Ольга",
+            LastName = "Плахина",
+            BirthDate = new DateTime(2007, 01, 01),
+            Grades = new List<Subject> { new Subject { Name = "ООП", Grade = 5 } }
+        };
+
+        try
+        {
+            StudentSerializer.SaveToFile(tempFile, student);
+            var loadedStudent = StudentSerializer.LoadFromFile(tempFile);
+
+            Assert.Equal(student.FirstName, loadedStudent.FirstName);
+            Assert.Equal(student.LastName, loadedStudent.LastName);
+            Assert.Equal(student.BirthDate, loadedStudent.BirthDate);
+            Assert.Single(loadedStudent.Grades!);
+        }
+        finally
+        {
+            if (File.Exists(tempFile)) File.Delete(tempFile);
+        }
+    }
+
+    [Fact]
+    public void Console_Demonstration_Output()
+    {
+        var student = new Student
+        {
+            FirstName = "Иван",
+            LastName = "Иванов",
+            BirthDate = new DateTime(2005, 5, 12),
+            Grades = new List<Subject> 
+            { 
+                new Subject { Name = "Математика", Grade = 5 },
+                new Subject { Name = "Физика", Grade = 4 }
+            }
+        };
+        string json = StudentSerializer.Serialize(student);
+        
+        Assert.NotNull(json);
+        Assert.Contains("2005-05-12", json);
+    }
+}
